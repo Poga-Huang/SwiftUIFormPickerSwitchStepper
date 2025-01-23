af928fd7c8c2ba2ba430a6196ad6af5acf873563
poga07 <chris@168money.com.tw>
2025-01-23 17:09:31 +0800

    Initial Commit

---
+++ SwiftUIFormPickerSwitchStepper/SwiftUIFormPickerSwitchStepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormPickerSwitchStepperApp.swift
+//  SwiftUIFormPickerSwitchStepper
+//
+//  Created by 黃柏嘉 on 2025/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormPickerSwitchStepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
